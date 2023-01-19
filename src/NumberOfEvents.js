b3e330f43719ef044dbf91b0247586a038c6360f
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-01-18 21:34:34 -0400

    holy moly the errorAlert works

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -13,7 +13,7 @@ class NumberOfEvents extends Component {
     if (inputValue <= 0 || inputValue >= 33) {
       this.setState({
         eventCount: inputValue,
-        errorText: "please pick a number betwen 0 - 100",
+        errorText: "Please pick a number betwen 0 - 100",
       });
     } else {
       this.setState({

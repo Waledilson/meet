83b6e0e76ec2b96acccd4d0c230c6071b01d596b
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-01-20 19:53:50 -0400

    will it work now?

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { mockData } from "./mock-data";
-import NProgress from "nprogress";
 import axios from "axios";
+import NProgress from "nprogress";
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
@@ -8,7 +8,7 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
@@ -39,11 +39,13 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
+
   if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return data ? JSON.parse(events).events : [];
+    return data ? JSON.parse(data).events : [];
   }
+
   const token = await getAccessToken();
 
   if (token) {

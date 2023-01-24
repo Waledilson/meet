88e846eb0a52c106c6391390f2df993c10774259
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-01-24 13:44:23 -0400

    made chart wider

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ class App extends Component {
         <h4>Events in each city</h4>
 
         <ScatterChart
-          width={400}
+          width={800}
           height={400}
           margin={{
             top: 20,
